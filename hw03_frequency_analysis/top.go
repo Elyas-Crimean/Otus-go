c52e71630f45b26da632cca4b501dd65d355f0f2
Ilia Sobolev <ilia_v_s@yahoo.com>
2023-10-16 23:11:07 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,51 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type Counter struct {
+	word  string
+	count int
+}
+
+var (
+	startPunctuations = regexp.MustCompile(`^(\.|,|<|\/|:|!|>|-|;)+`)
+	endPunctuations   = regexp.MustCompile(`(\.|,|<|\/|:|!|>|-|;)+$`)
+)
+
+func substitute(in string) string {
+	out := startPunctuations.ReplaceAllString(in, "")
+	out = endPunctuations.ReplaceAllString(out, "")
+	out = strings.ToLower(out)
+	return out
+}
+
+func Top10(text string) []string {
+	words := strings.Fields(text)
+	table := make(map[string]int, 10)
+	for _, w := range words {
+		key := substitute(w)
+		if key == "" {
+			continue
+		}
+		table[key]++
+	}
+	sorted := make([]Counter, 0, len(table))
+	for w, c := range table {
+		sorted = append(sorted, Counter{word: w, count: c})
+	}
+	sort.Slice(sorted, func(i, j int) bool {
+		if sorted[i].count == sorted[j].count { // сравнение строк при совпадении сётчиков
+			return strings.Compare(sorted[i].word, sorted[j].word) < 0
+		}
+		return sorted[i].count > sorted[j].count // '>' большие в начале
+	})
+	result := []string{}
+	for i := 0; i < 10 && i < len(sorted); i++ {
+		result = append(result, sorted[i].word)
+	}
+	return result
 }
--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Change to true if needed.
-var taskWithAsteriskIsCompleted = false
+var taskWithAsteriskIsCompleted = true
 
 var text = `Как видите, он  спускается  по  лестнице  вслед  за  своим
 	другом   Кристофером   Робином,   головой   вниз,  пересчитывая
@@ -47,7 +47,15 @@ func TestTop10(t *testing.T) {
 	t.Run("no words in empty string", func(t *testing.T) {
 		require.Len(t, Top10(""), 0)
 	})
-
+	t.Run("small text", func(t *testing.T) {
+		expected := []string{
+			"он",
+			"она",
+			"ты",
+			"я",
+		}
+		require.Equal(t, expected, Top10("Я, ты, он, она."))
+	})
 	t.Run("positive test", func(t *testing.T) {
 		if taskWithAsteriskIsCompleted {
 			expected := []string{
