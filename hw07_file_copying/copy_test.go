acb25ee4cbecc36f96bd8097398cf86abce79072
Ilia Sobolev <ilia_v_s@yahoo.com>
2023-12-10 21:16:44 +0300

    HW07 is completed

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,19 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
+	t.Run("Offset over size", func(t *testing.T) {
+		err := Copy("testdata/input.txt", "/tmp/copy_test_out.txt", 10_000, 0)
+		require.ErrorIs(t, err, ErrOffsetExceedsFileSize)
+	})
+	t.Run("Offset over size", func(t *testing.T) {
+		err := Copy("/dev/zero", "/tmp/copy_test_out.txt", 0, 0)
+		require.ErrorIs(t, err, ErrUnsupportedFile)
+	})
 	// Place your code here.
 }
