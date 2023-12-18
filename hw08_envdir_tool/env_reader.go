c0af5c15a34cee703960a2b114748e22bd17b34a
Ilia Sobolev <ilia_v_s@yahoo.com>
2023-12-18 22:01:17 +0300

    HW08 is completed step 1

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"os"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +16,31 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	dirEntryes, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	env := make(Environment)
+	for _, entry := range dirEntryes {
+		if !entry.Type().IsRegular() {
+			continue
+		}
+		content, err := os.ReadFile(dir + string(os.PathSeparator) + entry.Name())
+		if err != nil {
+			continue
+		}
+		if len(content) == 0 {
+			env[entry.Name()] = EnvValue{NeedRemove: true}
+			continue
+		}
+		lines := strings.Split(string(content), "\n")
+		if len(lines) > 0 {
+			env[entry.Name()] = EnvValue{
+				Value: strings.ReplaceAll(
+					strings.TrimRight(lines[0], " \t"),
+					"\x00", "\n"),
+			}
+		}
+	}
+	return env, nil
 }
