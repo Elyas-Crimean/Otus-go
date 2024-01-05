21e2245c3263b4b13dfae469ddf0f1e40ad2a8c9
Ilia Sobolev <ilia_v_s@yahoo.com>
2024-01-05 17:52:21 +0300

    независимые тесты

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -8,25 +8,25 @@ import (
 )
 
 func TestReadDir(t *testing.T) {
-	var commonEnv Environment
-
 	t.Run("prepare", func(t *testing.T) {
 		dirEnv, err := ReadDir("./testdata/env")
 		require.NoError(t, err)
 		require.Positive(t, len(dirEnv))
-		commonEnv = dirEnv
 	})
 	t.Run("quoted", func(t *testing.T) {
+		commonEnv, _ := ReadDir("./testdata/env")
 		v, ok := commonEnv["HELLO"]
 		assert.True(t, ok)
 		assert.Equal(t, EnvValue{"\"hello\"", false}, v)
 	})
 	t.Run("newline", func(t *testing.T) {
+		commonEnv, _ := ReadDir("./testdata/env")
 		v, ok := commonEnv["FOO"]
 		assert.True(t, ok)
 		assert.Equal(t, EnvValue{"   foo\nwith new line", false}, v)
 	})
 	t.Run("zero file", func(t *testing.T) {
+		commonEnv, _ := ReadDir("./testdata/env")
 		v, ok := commonEnv["UNSET"]
 		assert.True(t, ok)
 		assert.True(t, v.NeedRemove)
