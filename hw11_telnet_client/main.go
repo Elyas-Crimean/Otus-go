5f236c2855825fbdc1d48ba1c65d54ed62650858
Ilia Sobolev <ilia_v_s@yahoo.com>
2024-01-22 05:53:55 +0300

    HW11 test pass

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,63 @@
 package main
 
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+var timeout time.Duration
+
 func main() {
+	flag.DurationVar(&timeout, "timeout", time.Second*10, "возможная задержка подключения")
+	flag.Parse()
+	client := NewTelnetClient(net.JoinHostPort(flag.Arg(0), flag.Arg(1)), timeout, os.Stdin, os.Stdout)
+	err := client.Connect()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	go func() {
+		for {
+			err := client.Send()
+			if errors.Is(err, io.EOF) {
+				fmt.Fprintln(os.Stderr, "...EOF")
+				os.Exit(0)
+			}
+			if errors.Is(err, syscall.EPIPE) {
+				fmt.Fprintln(os.Stderr, "...Connection was closed by peer")
+				os.Exit(0)
+			}
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+	}()
+	go func() {
+		for {
+			err := client.Receive()
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+	}()
+	sigCtx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	sigChannel := sigCtx.Done()
+	<-sigChannel
+	stop()
+	client.Close()
+	os.Exit(1)
+
 	// Place your code here,
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"net"
 	"time"
 )
 
@@ -11,10 +12,41 @@ type TelnetClient interface {
 	Send() error
 	Receive() error
 }
+type tcImpl struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
-	return nil
+	return &tcImpl{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (tc *tcImpl) Connect() error {
+	var err error
+	tc.conn, err = net.DialTimeout("tcp", tc.address, tc.timeout)
+	return err
+}
+
+func (tc *tcImpl) Close() error {
+	return tc.conn.Close()
+}
+
+func (tc *tcImpl) Receive() error {
+	_, err := io.Copy(tc.out, tc.conn)
+	return err
+}
+
+func (tc *tcImpl) Send() error {
+	_, err := io.Copy(tc.conn, tc.in)
+	return err
 }
 
 // Place your code here.
